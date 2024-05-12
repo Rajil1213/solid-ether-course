675fd89cc8f73e74b4fca69bc854630fc7d069c5
Rajil Bajracharya <imrb1213@gmail.com>
2024-05-12 23:36:41 +0545

    chore: forge init

---
+++ lottery/lottery.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25 .0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable  {
+        require(msg.value >= 0.01 ether, "Ethers insufficient, expected: >= 0.01 ETH");
+        players.push(msg.sender);
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted {
+	    uint index = random() % players.length;
+	    address payable winnerAddress = payable(players[index]);
+	
+	    winnerAddress.transfer(address(this).balance);
+
+        // reset players list
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Only manager can pick the winner");
+        _;
+    }
+}
