ebb25ede8d2469c4b95e6eff480e3f2ae6cac9f0
Rajil Bajracharya <imrb1213@gmail.com>
2024-05-13 13:06:18 +0545

    tests: add test for enter

--- lottery/test/Lottery.t.sol
+++ lottery/test/Lottery.t.sol
@@ -14,4 +14,34 @@ contract LotteryTest is Test {
     function test_Initialization() public view {
         assertEq(lottery.getPlayers().length, 0);
     }
+
+    function testFail_EnterWithoutSufficientEthers() public {
+        address nonDeployerAddress = address(0);
+        vm.prank(nonDeployerAddress);
+
+        lottery.enter{value: 0.001 ether}();
+    }
+
+    function test_EnterWithSufficientEthers() public {
+        address player1 = makeAddr("non-deployer-1");
+        address player2 = makeAddr("non-deployer-2");
+
+        vm.startPrank(player1);
+        uint256 minEther = 0.01 ether;
+        vm.deal(player1, minEther);
+        lottery.enter{value: minEther}();
+        vm.stopPrank();
+
+        vm.startPrank(player2);
+        uint256 moreEther = 0.03 ether;
+        vm.deal(player2, moreEther);
+        lottery.enter{value: moreEther}();
+        vm.stopPrank();
+
+        assertEq(lottery.getPlayers().length, 2);
+        assertEq(lottery.getPlayers()[0], player1);
+        assertEq(lottery.getPlayers()[1], player2);
+
+        assertEq(address(lottery).balance, minEther + moreEther);
+    }
 }
